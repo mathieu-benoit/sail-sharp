4938e036d9296f2cc61550a78dfff94029312000
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-07-15 23:17:04 -0400

    Update Program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Builder;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var message = builder.Configuration.GetString("MESSAGE");
+var message = builder.Configuration.["MESSAGE"];
 
 app.MapGet("/", () => message ?? "Hello, World!");
 app.Run();
