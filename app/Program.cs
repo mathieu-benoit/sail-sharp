73d21213ac074638f13dd2197fa43da1c7863d09
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-10-11 19:27:14 -0400

    POD_NAME + NAMESPACE_NAME

--- app/Program.cs
+++ app/Program.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Builder;
 var builder = WebApplication.CreateSlimBuilder(args);
 var app = builder.Build();
 
-var message = builder.Configuration["MESSAGE"];
+var message = builder.Configuration["MESSAGE"] ?? "Hello, World!";
+var pod = builder.Configuration["POD_NAME"];
+var namespace = builder.Configuration["NAMESPACE_NAME"];
 
-app.MapGet("/", () => message ?? "Hello, World!");
+app.MapGet("/", () => string.IsNullOrEmpty(pod) || string.IsNullOrEmpty(namespace) ? message : $"{message} - from {pod} / {namespace}.");
 app.Run();
