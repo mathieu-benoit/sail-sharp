06c9189ae1dc0a1b1df246da4f6b00175ffcbdd5
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-10-11 19:42:40 -0400

    `POD_NAME` + `NAMESPACE_NAME` in Message (#81)
    * POD_NAME + NAMESPACE_NAME
    
    * podName + namespaceName

--- app/Program.cs
+++ app/Program.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Builder;
 var builder = WebApplication.CreateSlimBuilder(args);
 var app = builder.Build();
 
-var message = builder.Configuration["MESSAGE"];
+var message = builder.Configuration["MESSAGE"] ?? "Hello, World!";
+var podName = builder.Configuration["POD_NAME"];
+var namespaceName = builder.Configuration["NAMESPACE_NAME"];
 
-app.MapGet("/", () => message ?? "Hello, World!");
+app.MapGet("/", () => string.IsNullOrEmpty(podName) || string.IsNullOrEmpty(namespaceName) ? message : $"{message} - from {podName} / {namespaceName}.");
 app.Run();
