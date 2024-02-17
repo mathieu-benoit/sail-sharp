eb6fdbe5ae662096d5ead978ae000daf25eeafbe
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2024-02-17 20:10:11 +0000

    Optional config/appsettings.json

--- app/Program.cs
+++ app/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateSlimBuilder(args);
+builder.Configuration.AddJsonFile("config/appsettings.json", true, true);
+builder.Configuration.AddEnvironmentVariables();
 var app = builder.Build();
 
-var message = builder.Configuration["MESSAGE"] ?? "Hello, World!";
+var message = builder.Configuration["MESSAGE"] ?? "Hello, World! (from code)";
 var podName = builder.Configuration["POD_NAME"];
 var namespaceName = builder.Configuration["NAMESPACE_NAME"];
 
