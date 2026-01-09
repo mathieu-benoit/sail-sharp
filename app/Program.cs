ddd1d615072d50219b908f7e947d5ccd021bc7d1
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2026-01-08 20:33:00 -0500

    Add health checks endpoint to Program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -1,18 +1,14 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
-using System.Runtime.InteropServices;
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("appsettings.json", true, true);
 builder.Configuration.AddEnvironmentVariables();
+builder.Services.AddHealthChecks();
 var app = builder.Build();
-
 var message = builder.Configuration["MESSAGE"] ?? "Hello, World! (from code)";
 var podName = builder.Configuration["POD_NAME"];
 var namespaceName = builder.Configuration["NAMESPACE_NAME"];
 message = string.IsNullOrEmpty(podName) || string.IsNullOrEmpty(namespaceName) ? message : $"{message} - from {podName} / {namespaceName}";
 var platform = RuntimeInformation.OSArchitecture;
 var platformValue = platform == Architecture.Arm64 ? "arm64" : "amd64" ;
-
+app.MapHealthChecks("/healthz");
 app.MapGet("/", () => $"{message} on {platformValue}.");
 app.Run();
